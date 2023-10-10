235fa5bcc13043b8896b1b37bf1517ecb7169756
Alex Melander <Alexandermelander1@gmail.com>
2023-10-10 16:23:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// const text = document.querySelector(".text-one");
+// const textTwo = document.querySelector(".text-two");
+// const textThree = document.querySelector(".text-three");
+
+// const textWidth = text.offsetWidth;
+
+// function animateText(element) {
+//   gsap.to(element, {
+//     x: -textWidth,
+//     duration: 10,
+//     onComplete: function () {
+//       gsap.set(element, { x: textWidth });
+//       animateText(element);
+//     },
+//     ease: "linear",
+//   });
+// }
+
+// animateText(text);
+// animateText(textTwo);
+// animateText(textThree);
+
+// gsap.to(text, {
+//   x: "-100%",
+//   duration: 20,
+//   repeat: -1,
+//   ease: "linear",
+// });
+
+// gsap.to(textTwo, {
+//   x: "100%",
+//   duration: 20,
+//   repeat: -1,
+//   ease: "linear",
+// });
+
+// gsap.to(textThree, {
+//   x: "-100%",
+//   duration: 20,
+//   repeat: -1,
+//   ease: "linear",
+// });
+// 2;
